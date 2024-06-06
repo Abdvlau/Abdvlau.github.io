584801cea38d95d2f7badd490aa9f626165d1628
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-06-06 18:41:05 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.html',
-    './src/**/*.{html,js,jsx,ts,tsx,vue}'
+    './index.html',
+    './css/**/*.css',
+    './js/**/*.js',
   ],
   theme: {
-    extend: {
-      colors: {
-        'blue-950': '#1E3A8A',
-        'blue-900': '#1D4ED8',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
